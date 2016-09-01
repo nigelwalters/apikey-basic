bb2439e052517e72761ea9549803f0eb7c5b070a
Nigel Walters <nwalters@apigee.com>
2016-09-01 13:14:45 +0100

    realigning with sample

--- make.sh
+++
@@ -1,1 +0,0 @@
-apigeetool deployproxy -e $env -u $ae_username -p $ae_password -o $org -n 'apikey-basic'
--- test/test.js
+++ test/test.js
@@ -1,14 +1,17 @@
-
-// var request = require('request')
-// var async = require('async')
-
-// var org = process.env.ORG
-// var env = process.env.ENV
 var assert = chai.assert;
-var org = 'amer-demo4'
-var env = 'test'
 
-var url = 'http://' + org + '-' + env + '.apigee.net/quota-basic'
+var pathname = window.location.pathname
+var splits = pathname.split('/')
+//expected /v1/o/:orgname/e/:env/samples/:sample/test.html
+var org = splits[3]
+var env = splits[5]
+var sample = splits[7]
+
+getTestData(org,env,sample,function(data){
+	//i don have any data for this proxy
+})
+
+var url = 'https://' + org + '-' + env + '.apigee.net/quota-basic'
 describe('QuotaBasic', function(){
 	describe('calling ' + url, function(){
 		it('make 5 API calls, only 2 should succeed', function(done){
@@ -18,8 +21,8 @@ describe('QuotaBasic', function(){
 					complete:function(xhr,statusText){ next(null,xhr.status)}
 				})
 			},function(cberror,codes){
-				var success_200 = 0				
-				codes.forEach(function(s){ if (s==200) success_200++}) 
+				var success_200 = 0
+				codes.forEach(function(s){ if (s==200) success_200++})
 				assert.equal(2,success_200)
 				done(cberror)
 			})
